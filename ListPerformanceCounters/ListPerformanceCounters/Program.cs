2271647e20bbce8dad5d443d1e50afa282e0f451
Roger Cruz <roger_r_cruz@@yahoo.com>
2019-01-12 12:39:50 -0500

    New project to list performance counter categories on current computer

---
+++ ListPerformanceCounters/ListPerformanceCounters/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+
+// Copyright Roger Cruz (roger_r_cruz@@yahoo.com)
+namespace ListPerformanceCounters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Inspired by https://docs.microsoft.com/en-us/dotnet/api/system.diagnostics.performancecountercategory.getcategories?redirectedfrom=MSDN&view=netframework-4.7.2#overloads
+            PerformanceCounterCategory[] categories;
+            categories = PerformanceCounterCategory.GetCategories();
+            foreach (var category in categories)
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+            Console.WriteLine("Press key to exit");
+            Console.ReadLine();
+
+        }
+    }
+}
