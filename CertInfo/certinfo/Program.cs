dd74513d6fa8c4da0f0163547f021144ef8918e6
Roger Cruz <roger.cruz@citrix.com>
2020-12-24 20:05:20 -0500

    CertInfo source

--- CertInfo/certinfo/Program.cs
+++ CertInfo/certinfo/Program.cs
@@ -1,11 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
 
+
+// Original source
 namespace certinfo
 {
     class CertInfo
